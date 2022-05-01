b70878af2f5b19ce40bc5879564f40c3cadec4d7
albinassorys <99352009+albinassorys@users.noreply.github.com>
2022-05-01 11:54:52 +0300

    Add files via upload

---
+++ Task 6/script.js
@@ -0,0 +1,47 @@
+/* ------------------------------ TASK 6 -----------------------------------
+Turimas "users" masyvas. 
+
+Parašykite funckijas, kurios atlikas nurodytas užduotis
+1. funkcija "getUserAverageAge" - kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" amžiaus visurkį kaip skaičių.
+2. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
+atveju grąžins visų "users" vardus naujame masyve pvz., ['John Smith', 'Ann Smith'..].
+-------------------------------------------------------------------------- */
+
+const users = [
+  { id: '1', name: 'John Smith', age: 20 },
+  { id: '2', name: 'Ann Smith', age: 24 },
+  { id: '3', name: 'Tom Jones', age: 31 },
+  { id: '4', name: 'Rose Peterson', age: 17 },
+  { id: '5', name: 'Alex John', age: 25 },
+  { id: '6', name: 'Ronald Jones', age: 63 },
+  { id: '7', name: 'Elton Smith', age: 16 },
+  { id: '8', name: 'Simon Peterson', age: 30 },
+  { id: '9', name: 'Daniel Cane', age: 51 },
+];
+
+function getUserAverageAge(){
+
+let averageAge = 0;
+
+users.forEach(item => averageAge+= item.age);
+
+console.log(`Suapvalintas amžiaus vidurkis yra ${Math.round(averageAge/users.length)}`)
+
+}
+
+getUserAverageAge()
+
+function getUsersNames(){
+
+const names = [];
+
+users.forEach(item => names.push(item.name))
+
+console.log(names)
+
+}
+
+getUsersNames()
+
+
