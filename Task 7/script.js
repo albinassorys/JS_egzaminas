1daf7bf3ef7f747411dae1595f9c29e027b7d1d8
albinassorys <99352009+albinassorys@users.noreply.github.com>
2022-05-03 17:40:53 +0300

    Add files via upload

---
+++ Task 7/script.js
@@ -0,0 +1,23 @@
+/* ------------------------------ TASK 7 -----------------------------------
+Turimas "audi" objektas.
+
+Parašykite funkciją showObjectKeys, kuri kaip argumentą priims objectą 
+ir grąžins visus jo "key" masyve.
+-------------------------------------------------------------------------- */
+const audi = {
+  make: 'audi',
+  model: 'A6',
+  year: 2005,
+  color: 'white',
+};
+
+
+function showObjectKeys(){
+
+
+let keys = Object.keys(audi);
+return keys;
+
+}
+
+console.log(showObjectKeys());
